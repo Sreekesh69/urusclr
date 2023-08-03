beebfb6c145662f43c33f47a8889b0c8d56f614d
Sreekesh69 <138349226+Sreekesh69@users.noreply.github.com>
2023-08-03 10:57:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("black").addEventListener('click', bf2);
+function bf2(){
+document.getElementById("img1").src="https://yourtestdriver.com/wp-content/uploads/2022/06/2a2d0c89-aa71-41b9-b376-2f3f8f330bcf.jpg";
+}
+document.getElementById("orange").addEventListener('click', bf3);
+function bf3(){
+document.getElementById("img1").src="https://etimg.etb2bimg.com/photo/76442121.cms";
+}
+document.getElementById("green").addEventListener('click', bf1);
+function bf1(){
+document.getElementById("img1").src="https://static.wixstatic.com/media/d94c66_1bd26619e6444752b5a3a84b883a9126~mv2.jpeg/v1/fill/w_980,h_653,al_c,q_85/d94c66_1bd26619e6444752b5a3a84b883a9126~mv2.jpeg";
+}
+document.getElementById("add").addEventListener('click',bf4);
+function bf4(){
+    document.getElementById("add").innerHTML="Your Item Added to Cart";
+}
